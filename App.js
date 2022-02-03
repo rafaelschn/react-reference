7591b3ac84cde4779ca7be2c2c8e7aab5d36d22b
rafaelschn <schn.rfl@gmail.com>
2022-02-03 20:58:43 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { Provider } from 'react-redux';
+import store from './store';
 import Header from './components/Header';
+import UserProvider from "./contexts/user";
 import './estilo.css';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <hr />
-      <Outlet />
-    </div>
+    <Provider store={store}>
+      <UserProvider>
+        <div>
+          <Header />
+          <hr />
+          <Outlet />
+        </div>
+      </UserProvider>
+    </Provider>
   );
 }
 
